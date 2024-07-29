d3dc4010712d23899fd60f30219f7f062761b698
Mina Adel <minallegend@gmail.com>
2024-07-29 17:06:11 +0300

    custom path

--- src/Server.cs
+++ src/Server.cs
@@ -23,12 +23,15 @@ var path = requestLine[1];
 var httpVer = requestLine[2];
 
 // Determine the response based on the requested path
-if (path == "/") {
-    // Send a 200 OK response
-    var response = "HTTP/1.1 200 OK\r\n\r\n";
+if (path.StartsWith("/echo/")) {
+    // Extract the string from the path
+    var echoStr = path.Substring(6);
+    // Construct the response
+    var responseBody = echoStr;
+    var response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {responseBody.Length}\r\n\r\n{responseBody}";
     socket.Send(Encoding.UTF8.GetBytes(response));
 } else {
-    // Send a 404 Not Found response
+    // Send a 404 Not Found response for any other path
     var response = "HTTP/1.1 404 Not Found\r\n\r\n";
     socket.Send(Encoding.UTF8.GetBytes(response));
 }
